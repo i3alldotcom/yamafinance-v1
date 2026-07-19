7c1b1da0599c2f05013ebfd2e6389d19283fd513
i3alldotcom <127671814+i3alldotcom@users.noreply.github.com>
2026-07-19 20:18:44 +0900

    Update line-webhook.js

--- api/line-webhook.js
+++ api/line-webhook.js
@@ -2,11 +2,14 @@ export default async function handler(req, res) {
   console.log("LINE Webhook:", req.body);
 
   // ส่งข้อมูลไป NAS
-  await fetch("http://dmcyamanashi.myqnapcloud.com/yamafinance/public/api/line.php", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ test: "hello from vercel" })
-  });
+  await fetch("https://dmcyamanashi.myqnapcloud.com/api/line.php", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "X-API-KEY": "Yama072+Finance@2026"
+  },
+  body: JSON.stringify(req.body)
+});
 
   res.status(200).send("OK");
 }
